8e39fead167b9b7a620544ea074a30a291892120
Maratrom <arthur.lagneaux@gmail.com>
2026-03-11 10:20:44 +0100

    feat(service/download): Add File Download Service
    - Add `Merci.gif` to the public folder
    - Add the `download` service
    - Add a method in the `Contact` component to trigger the download
    - Add a call to this method on form submit button click
    
    Changelog: added

--- elie2027/src/app/components/contact/contact.ts
+++ elie2027/src/app/components/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AvisComponent } from '../avis/avis';
+import { DownloadService } from '../../services/download';
 @Component({
   selector: 'app-contact',
   imports: [FormsModule, AvisComponent],
@@ -8,6 +9,8 @@ import { AvisComponent } from '../avis/avis';
   styleUrl: './contact.scss',
 })
 export class Contact {
+
+  constructor(private downloadService: DownloadService) { }
 
   name = signal('');
   email = signal('');
@@ -20,5 +23,9 @@ export class Contact {
     this.email.set('');
     this.message.set('');
     setTimeout(() => this.submitted.set(false), 4000);
+  }
+
+  downloadGif() {
+    this.downloadService.download('/Merci.gif', 'Merci.gif');
   }
 }
